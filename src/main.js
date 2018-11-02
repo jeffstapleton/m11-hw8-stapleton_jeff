58ae14704c6cc1d9c9d98db99193aa8082336623
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-11-02 17:05:59 -0500

    Create includeSpoon method

--- src/main.js
+++ src/main.js
@@ -7,7 +7,15 @@ class Desserts {
 
 class IceCream extends Desserts {
   constructor (type, calories, flavor, scoops){
-    
+    super(type, calories);
+    this.flavor = flavor;
+    this.scoops = scoops;
   }
+  includeSpoon(){
+    if(this.scoops === 3) {
+      console.log('Here is your spoon!');
+    }
+  }
+
 }
 
